0d88842f32b6089c75ba9f64c3749c02ec606d66
Dmitriy Zhirko <zhirkod@gmail.com>
2019-09-11 12:06:23 +0300

    Changes in functional_test

---
+++ functional_test.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+	'''тест нового посетителя'''
+
+	def setUp(self):
+		'''установка'''
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		'''демонтаж'''
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		'''тест: можно начать список и получить его позже'''
+		self.browser.get('http://localhost:8000')
+		self.assertIn('TO-DO', self.browser.title)
+		self.fail('Закончить тест!')
+
+if __name__ == '__main__':
+	unittest.main()
