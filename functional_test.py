c610cce24a3d0b677c27486efd45920a93db8f0d
Dmitriy Zhirko <zhirkod@gmail.com>
2019-09-12 13:30:14 +0300

    HTML главной страницы теперь генерируется из шаблона

--- functional_test.py
+++ functional_test.py
@@ -17,19 +17,20 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		'''тест: можно начать список и получить его позже'''
 		self.browser.get('http://localhost:8000')
-		self.assertIn('TO-DO', self.browser.title)
+		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
-			inputbox.get_attribute('placeholder'), 'Enter a to do item')
+			inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
 		inputbox.send_keys('Купить павлиньи перья')
 		time.sleep(1)
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Купить павлиньи перья' for row in rows)
+			any(row.text == '1: Купить павлиньи перья' for row in rows),
+			"Новый элемент списка не появился в таблице"
 		)
 		self.fail('Закончить тест!')
 
