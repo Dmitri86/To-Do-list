f83c2c596670ad9676d85e9091d19ebcd8cc5798
Dmitriy Zhirko <zhirkod@gmail.com>
2019-09-13 17:24:43 +0300

    сделать functional_tests приложением, использовать LiveServerTestCase

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,9 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 	'''тест нового посетителя'''
 
 	def setUp(self):
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		'''тест: можно начать список и получить его позже'''
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
@@ -36,7 +36,7 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 		self.check_for_row_in_list_table('1: Купить павлиньи перья')
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Сделать мушку из павлинных перьев')
+		inputbox.send_keys('Сделать мушку из павлиньих перьев')
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 		self.check_for_row_in_list_table('1: Купить павлиньи перья')
