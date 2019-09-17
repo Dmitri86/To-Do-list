413b36785f081d928647d7430a617da7f866296d
Dmitriy Zhirko <zhirkod@gmail.com>
2019-09-17 12:35:52 +0300

    changed test and views

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from lists.models import Item
+from lists.models import Item, List
 from django.shortcuts import render, redirect
 # Create your views here.
 
@@ -14,5 +14,6 @@ def view_list(request):
 
 def new_list(request):
 	'''новый список'''
-	Item.objects.create(text=request.POST['item_text'])
+	list_ = List.objects.create()
+	Item.objects.create(text=request.POST['item_text'], list=list_)
 	return redirect(view_list)
